705715e934a3cf5fa8146ed9fdb0e22466c3f183
ridwanharts <ridwanharts@gmail.com>
2024-12-18 17:18:10 +0700

    first commit

---
+++ src/main/java/com/ridwanharts/report_card/ReportCardApplication.java
@@ -0,0 +1,13 @@
+package com.ridwanharts.report_card;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportCardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ridwanharts/report_card/ReportCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ridwanharts.report_card;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
